b64b19a3132affa847b01541bc9218f052909d67
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-08 23:03:49 +0200

    work with database

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -5,23 +8,52 @@ export enum TopLevelCategory {
   Products,
 }
 
-export class TopPageModel {
-  _id;
-  firstLevelCategory: TopLevelCategory;
-  secondLevelCategory: string;
+export class hhData {
+  @prop()
+  count: number;
+  @prop()
+  juniorSalary: number;
+  @prop()
+  middleSalary: number;
+  @prop()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantage {
   title: string;
+  description: string;
+}
+
+export interface TopPageModel extends Base {}
+
+export class TopPageModel extends TimeStamps {
+  @prop({ enum: TopLevelCategory })
+  firstLevelCategory: TopLevelCategory;
+
+  @prop()
+  secondLevelCategory: string;
+
+  @prop({ unique: true })
+  alias: string;
+
+  @prop()
+  title: string;
+
+  @prop()
   category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+
+  @prop({ type: () => hhData })
+  hh?: hhData;
+
+  @prop({ type: () => [TopPageAdvantage] })
+  advantages: TopPageAdvantage[];
+
+  @prop()
   seoText: string;
+
+  @prop()
   tagsTitle: string;
+
+  @prop({ type: () => [String] })
   tags: string[];
 }
